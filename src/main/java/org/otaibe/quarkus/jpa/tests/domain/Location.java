df65493e5561135af3fa67d233ccca659e3ae9ef
Triphon Penakov <triphon@gmail.com>
2020-02-25 15:41:09 +0200

    Location - added get/set for inherited field name

--- src/main/java/org/otaibe/quarkus/jpa/tests/domain/Location.java
+++ src/main/java/org/otaibe/quarkus/jpa/tests/domain/Location.java
@@ -16,4 +16,14 @@ import javax.persistence.Id;
 public class Location extends LocationBase {
     @Id
     private Long id;
+
+    @Override
+    public String getName() {
+        return super.getName();
+    }
+
+    @Override
+    public void setName(String name) {
+        super.setName(name);
+    }
 }
